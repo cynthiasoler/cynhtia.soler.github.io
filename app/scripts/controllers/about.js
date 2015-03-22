8a0cbf77803aa8b5dbb2f64beda622963557db5c
edouard <souan.edouard0@gmail.com>
2015-03-22 19:40:51 +0100

    init commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cynhtiasolergithubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cynhtiasolergithubioApp
+ */
+angular.module('cynhtiasolergithubioApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cynhtiasolergithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cynhtiasolergithubioApp
+ */
+angular.module('cynhtiasolergithubioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
