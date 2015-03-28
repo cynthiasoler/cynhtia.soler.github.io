d43af2fbd2eb728b9744d694e978853b70f0117f
edouard <souan.edouard0@gmail.com>
2015-03-28 23:36:49 +0100

    Update main view

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -1,3 +1,16 @@
 var app = angular.module("portfolio", [
+    'ngRoute',
     'ngMaterial'
 ]);
+
+
+app.config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {templateUrl: 'view/main.html', controller: 'MainCtrl'})
+        .when('/experiences', {templateUrl: 'view/experiences.html', controller: 'ExpCtrl'})
+        .when('/portfolio', {templateUrl: 'view/portfolio.html', controller: 'PortfolioCtrl'});
+
+
+
+    $locationProvider.html5Mode(true);
+}]);
--- client/app/js/controller/mainController.js
+++ client/app/js/controller/mainController.js
@@ -1,7 +1,12 @@
-angular.module("portfolio").controller("MainCtrl", function ($scope) {
+angular.module("portfolio").controller("MainCtrl", function ($scope, $route, $routeParams, $location) {
+    this.$route = $route;
+    this.$location = $location;
+    this.$routeParams = $routeParams;
+
     $scope.identity = {
         lastName: "Soler",
         firstName: "Cynthia"
     };
 
+    $scope.mainView = "view/main.html";
 });
