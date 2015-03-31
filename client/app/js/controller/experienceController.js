fac89d8f0cabfb3e6ad8fa863f0b84f06dc6a594
edouardsouan <souan.edouard0@gmail.com>
2015-03-31 11:05:17 +0200

    Add content to experiences.html

--- client/app/js/controller/experienceController.js
+++ client/app/js/controller/experienceController.js
@@ -1,4 +1,25 @@
 angular.module('portfolio').controller('ExpCtrl', function ($scope, $routeParams) {
-    $scope.exp = "Mes expériences";
-    this.params = $routeParams;
+    $scope.exps = [{
+        what: 'team Work',
+        notes: 'Sometimes it was hard'
+    },{
+        what: 'ijrvbeqlihvbe',
+        notes: 'jvbvhbtihb'
+    },{
+        what: 'ijrvbeqlihvbe',
+        notes: 'jvbvhbtihb'
+    },{
+        what: 'ijrvbeqlihvbe',
+        notes: 'jvbvhbtihb'
+    },{
+        what: 'ijrvbeqlihvbe',
+        notes: 'jvbvhbtihb'
+    },{
+        what: 'ijrvbeqlihvbe',
+        notes: 'jvbvhbtihb'
+    },{
+        what: 'ijrvbeqlihvbe',
+        notes: 'jvbvhbtihb'
+    }
+    ];
 });
