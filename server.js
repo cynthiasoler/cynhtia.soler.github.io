056613ca29f44df096b97988c5da199af11afc4c
Edouard Souan <souan.edouard0@gmail.com>
2015-08-02 18:32:50 +0200

    Fixed server serving file on public repository

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ var router = require('./server/router/mainRouter.js');
 var mongoose = require('mongoose');
 var Constants = require('./server/constants.json');
 var bodyParser = require('body-parser');
+var path = require("path");
 
 app = express();
 mongoose.connect(Constants.mongoURI);
 
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));;
+app.all('/', function (req, res) {
+    res.sendFile('index.html', { root: path.join(__dirname, '/public/') });
+});
 
 app.use('/api', router);
 
-var server = app.listen(process.env.PORT || 8090, console.log('Server listening on port : 8090'));
+app.listen(process.env.PORT || 8090, console.log('Server listening on port : 8090'));
